b97314e1bfc1ef0b112f67383fefa1347d1e00b5
songjunxi <3224266014@qq.com>
2023-04-24 13:02:15 +0800

    upd

--- components/home/photo-booth.tsx
+++ components/home/photo-booth.tsx
@@ -150,7 +150,7 @@ export default function PhotoBooth({
                       className="group mx-auto mt-6 flex max-w-fit items-center justify-center space-x-2 rounded-full border border-black bg-black px-5 py-2 text-sm text-white transition-colors hover:bg-white hover:text-black"
                       onClick={() => setShowUploadModal(true)}
                     >
-                      <Upload className="h-5 w-5 text-white group-hover:text-black" />
+                      <Upload className="h-5 w-5 text-white" />
                       <p>重新上传</p>
                     </button>
                   </div>
