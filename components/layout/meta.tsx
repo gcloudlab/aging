1187c20065978d406dd1d00e4aeba9c54ddb3f36
yesmore <3224266014@qq.com>
2023-04-24 20:54:02 +0800

    fix

--- components/layout/meta.tsx
+++ components/layout/meta.tsx
@@ -1,5 +1,20 @@
 import Head from "next/head";
 
+export const defaultMetaProps = {
+  title: 'Aging - Oh my life',
+  description:
+    '只需上传一张你现在的照片，Aging 将利用人工智能技术推演出你从幼年到老年的面容变化。',
+  ogImage: ``,
+  ogUrl: 'https://ai.aging.run'
+};
+
+export interface MetaProps {
+  title: string;
+  description: string;
+  ogUrl?: string;
+  ogImage?: string;
+}
+
 export default function Meta({
   title = "Aging - Oh my life",
   description = "只需上传一张你现在的照片，Aging 将利用人工智能技术推演出你从幼年到老年的面容变化。",
