4454926af9d6f490f39422e8d67219e050d690cc
yesmore <3224266014@qq.com>
2023-04-25 21:37:31 +0800

    upd

--- pages/[username].tsx
+++ pages/[username].tsx
@@ -5,6 +5,11 @@ import { MetaProps, defaultMetaProps } from "@/components/layout/meta";
 import { getUser, getAllUsers, getUserCount, UserProps } from "@/lib/api/user";
 // export { default } from ".";
 import clientPromise from "@/lib/mongodb";
+import { useState } from "react";
+import { GitHubIcon, LoadingDots } from "@/components/icons";
+import Avatar from "@/components/avatar";
+import { motion } from "framer-motion";
+import { signIn, signOut, useSession } from "next-auth/react";
 
 interface Params extends ParsedUrlQuery {
   username: string;
@@ -18,10 +23,66 @@ export default function Username({
   user: UserProps;
 }) {
   // console.log("[username]", user);
+  const { data: session, status } = useSession();
+  const [loading, setLoading] = useState(false);
 
   return (
     <Layout>
-      <div className="z-10">Hi, {user?.name}</div>
+      <motion.div
+        className="z-10 "
+        initial="hidden"
+        whileInView="show"
+        animate="show"
+        viewport={{ once: true }}
+        variants={{
+          hidden: {},
+          show: {
+            transition: {
+              staggerChildren: 0.15,
+            },
+          },
+        }}
+      >
+        {status !== "loading" &&
+          (session?.user ? (
+            <div className="flex flex-col items-center justify-center">
+              <Avatar />
+              <div className="mt-3 w-48">
+                {session?.user && (
+                  <button
+                    onClick={() => {
+                      signOut();
+                    }}
+                    className={`h-10 w-48 rounded-md border bg-white px-2 py-1 text-sm text-black transition-all hover:bg-black hover:text-white`}
+                  >
+                    Sign out
+                  </button>
+                )}
+              </div>
+            </div>
+          ) : (
+            <button
+              disabled={loading}
+              onClick={() => {
+                setLoading(true);
+                signIn("github", { callbackUrl: `/` });
+              }}
+              className={`${
+                loading
+                  ? "border-gray-300 bg-gray-200"
+                  : "border-black bg-black hover:bg-gray-100"
+              } h-10 w-48 rounded-md border px-2 py-1 text-sm text-white transition-all hover:text-black`}
+            >
+              {loading ? (
+                <LoadingDots color="gray" />
+              ) : (
+                <div className="flex items-center justify-center">
+                  <GitHubIcon className="mr-2 h-5" /> Sign in with GitHub
+                </div>
+              )}
+            </button>
+          ))}
+      </motion.div>
     </Layout>
   );
 }
